8eb958e406ba1fc6a57ef43c6bceb9e95039824d
joy <joy.jitae@gmail.com>
2020-06-23 22:18:00 +0900

    s

--- src/main/java/com/springboot/demo/HelloController.java
+++ src/main/java/com/springboot/demo/HelloController.java
@@ -1,13 +1,26 @@
 package com.springboot.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello Spring-Boot World!";
+        return "index";
     }
+
+   /* @RequestMapping("/{num}")
+    public String index(@PathVariable int num) {
+        int res = 0;
+        for(int i = 1; i<= num; i++){
+            res += i;
+        }
+        return "total:" + res;
+//        return "Hello Spring-Boot World!";
+    }*/
+
 }
