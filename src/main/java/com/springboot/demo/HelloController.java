7e3c1551f61f46a878aa7288fd64583e1c2520fb
joy <joy.jitae@gmail.com>
2020-06-24 21:23:18 +0900

    Annotation and thymeleaf 를 이용

--- src/main/java/com/springboot/demo/HelloController.java
+++ src/main/java/com/springboot/demo/HelloController.java
@@ -1,17 +1,36 @@
 package com.springboot.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "index";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index(ModelAndView mav) {
+        mav.setViewName("index");
+        mav.addObject("msg", "이름을 적어서 전송해주세요.");
+        return mav;
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ModelAndView send(@RequestParam("text1") String str, ModelAndView mav){
+        mav.addObject("msg", "안녕하세여" + str);
+        mav.addObject("value", str);
+        mav.setViewName("index");
+        return mav;
+    }
+    /*@RequestMapping("/{num}")
+    public String index(@PathVariable int num, Model model) {
+        int res = 0;
+        for(int i = 0; i <= num; i++){
+            res += i;
+        }
+        model.addAttribute("msg", "total : " + res);
+        return "index";
+    }*/
 
    /* @RequestMapping("/{num}")
     public String index(@PathVariable int num) {
