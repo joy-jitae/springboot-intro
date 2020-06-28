5ceee36da747533dcbb7ffbe71bdede3a55f93a5
joy <joy.jitae@gmail.com>
2020-06-28 20:25:57 +0900

    @ModelAttribute 와 데이터 조장

--- src/main/java/com/springboot/demo/HelloController.java
+++ src/main/java/com/springboot/demo/HelloController.java
@@ -1,27 +1,43 @@
 package com.springboot.demo;
 
+import com.springboot.demo.repositories.MyDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
+    @Autowired
+    MyDataRepository repository;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView index(ModelAndView mav) {
+    public ModelAndView index(@ModelAttribute("formModel") MyData myData, ModelAndView mav) {
         mav.setViewName("index");
-        mav.addObject("msg", "이름을 적어서 전송해주세요.");
+        mav.addObject("msg", "this is sample content.");
+        Iterable<MyData> list = repository.findAll();
+        mav.addObject("datalist", list);
         return mav;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public ModelAndView form(@ModelAttribute("formModel") MyData myData, ModelAndView mav) {
+        repository.saveAndFlush(myData);
+        return new ModelAndView("redirect:/");
+    }
+
+    /*@RequestMapping(value = "/", method = RequestMethod.POST)
     public ModelAndView send(@RequestParam("text1") String str, ModelAndView mav){
         mav.addObject("msg", "안녕하세여" + str);
         mav.addObject("value", str);
         mav.setViewName("index");
         return mav;
-    }
+    }*/
     /*@RequestMapping("/{num}")
     public String index(@PathVariable int num, Model model) {
         int res = 0;
