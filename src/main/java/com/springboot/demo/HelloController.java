fab027f5f202f469c3815edd0ed20b5a00a50de5
jhyu <jhyu@wemb.co.kr>
2020-07-01 22:47:12 +0900

    findById method 로 사용

--- src/main/java/com/springboot/demo/HelloController.java
+++ src/main/java/com/springboot/demo/HelloController.java
@@ -4,16 +4,35 @@ import com.springboot.demo.repositories.MyDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class HelloController {
 
     @Autowired
     MyDataRepository repository;
+
+    public void init() {
+        MyData d1 = new MyData();
+        d1.setName("YU");
+        d1.setAge(28);
+        d1.setMail("wlxosmsla");
+        d1.setMemo("This is my data!!");
+        repository.saveAndFlush(d1);
+        MyData d2 = new MyData();
+        d2.setName("JI");
+        d2.setAge(28);
+        d2.setMail("wlxosmsla");
+        d2.setMemo("This is my data!!");
+        repository.saveAndFlush(d2);
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView index(@ModelAttribute("formModel") MyData myData, ModelAndView mav) {
@@ -31,6 +50,22 @@ public class HelloController {
         return new ModelAndView("redirect:/");
     }
 
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public ModelAndView edit(@ModelAttribute MyData myData, @PathVariable int id, ModelAndView mav){
+        mav.setViewName("edit");
+        mav.addObject("title", "edit mydata.");
+        MyData data = repository.findById((long)id);
+        mav.addObject("formModel2", data);
+
+        return mav;
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public ModelAndView update(@ModelAttribute MyData myData, ModelAndView mav) {
+        repository.saveAndFlush(myData);
+        return new ModelAndView("redirect:/");
+    }
     /*@RequestMapping(value = "/", method = RequestMethod.POST)
     public ModelAndView send(@RequestParam("text1") String str, ModelAndView mav){
         mav.addObject("msg", "안녕하세여" + str);
