462e63cf6ee67d5467f424aaeb7fc348a376bcde
joy <joy.jitae@gmail.com>
2020-08-02 23:48:25 +0900

    createQuery를 이용한 Query 작성, EntitylManager의 createQuery는 인수에 JPQL 쿼리문을 지정해서 호출

--- src/main/java/com/springboot/demo/MyDataDaoImpl.java
+++ src/main/java/com/springboot/demo/MyDataDaoImpl.java
@@ -2,6 +2,30 @@ package com.springboot.demo;
 
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
 @Repository
-public class MyDataDaoImpl {
+public class MyDataDaoImpl implements MyDataDao<MyData>{
+
+    private static final long serialVersionUID = 1L;
+
+    private EntityManager entityManager;
+
+    public MyDataDaoImpl() {
+        super();
+    }
+
+    public MyDataDaoImpl(EntityManager manager){
+        entityManager = manager;
+    }
+
+    @Override
+    public List<MyData> getAll() {
+        Query  query = entityManager.createQuery("from MyData");
+        List<MyData> list = query.getResultList();
+        entityManager.close();
+        return list;
+    }
 }
